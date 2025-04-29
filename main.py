9e860a2437d8e9f53f1bc753b56ef01a403c9176
thomas4545 <tbarron@constructa.fr>
2025-04-29 10:49:17 +0200

    nex

--- main.py
+++ main.py
@@ -51,4 +51,4 @@ async def extract_text(file: UploadFile = File(...)):
 # Permet de lancer directement l'application avec Python
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=10000, reload=True)
